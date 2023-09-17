7a63535660aa6c2f8d9c1d5e48b326be020c2b96
sefaunal <83312431+sefaunal@users.noreply.github.com>
2023-09-17 22:44:37 +0300

    JWT Implementation
    - JWT Implementation have been finished
    - Java version have been upgraded to 21
    - Swagger have been implemented

--- src/main/java/com/sefaunal/umbrellachat/UmbrellaChatApplication.java
+++ src/main/java/com/sefaunal/umbrellachat/UmbrellaChatApplication.java
@@ -1,8 +1,11 @@
 package com.sefaunal.umbrellachat;
 
+import com.sefaunal.umbrellachat.Config.RSA256Keys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RSA256Keys.class)
 @SpringBootApplication
 public class UmbrellaChatApplication {
 
