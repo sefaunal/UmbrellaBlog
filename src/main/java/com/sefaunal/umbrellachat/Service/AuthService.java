7fa3312a724ef0fe5d8076dd50d1a75a580f067a
sefaunal <83312431+sefaunal@users.noreply.github.com>
2023-09-17 22:44:37 +0300

    JWT Implementation
    - JWT Implementation have been finished
    - Java version have been upgraded to 21
    - Swagger have been implemented

---
+++ src/main/java/com/sefaunal/umbrellachat/Service/AuthService.java
@@ -0,0 +1,54 @@
+package com.sefaunal.umbrellachat.Service;
+
+import com.sefaunal.umbrellachat.Request.AuthenticationRequest;
+import com.sefaunal.umbrellachat.Request.RegisterRequest;
+import com.sefaunal.umbrellachat.Response.AuthenticationResponse;
+import com.sefaunal.umbrellachat.Model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author github.com/sefaunal
+ * @since 2023-09-18
+ **/
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserService userService;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JWTService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRole("USER");
+
+        userService.createUser(user);
+        String JWT = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(JWT).build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        User user = userService.findUserByMail(request.getEmail()).orElseThrow();
+        String JWT = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(JWT).build();
+    }
+}
